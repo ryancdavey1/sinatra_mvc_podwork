86c6268ad7152c355d8025bddda51ceb620640fe
Lantz Warrick <betalantz@gmail.com>
2020-07-27 16:40:41 -0700

    adds view and routes including fetch by author

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < Sinatra::Base
 
+    set :views, ->{File.join(root, "../views")}
+
+    get '/' do
+        erb :welcome
+    end
+
+    get '/books' do 
+        @books = Book.all
+        erb :index
+    end
+
+    get '/books/random' do 
+        @book = Book.all.sample
+        erb :random
+    end
+
+    post '/books/search' do
+        author = params[:author]
+        GoogleBooks::Adapter.new(author).fetch_books
+        redirect '/books'
+    end
+
+
 end
