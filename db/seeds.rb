547564c0160200a7e0ab2dcaea94ca24ffd51ec8
ryancdavey1 <ryancdavey1@gmail.com>
2020-07-30 17:32:26 -0400

    set up migration, seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 Book.destroy_all
 
 GoogleBooks::Adapter.new("Madeline L'Engle").fetch_books
+
+# book.rb -> book class, active record
+# migrate file -> creates book table
+# seeds.rb -> creates instances of books -> books saved to the database
